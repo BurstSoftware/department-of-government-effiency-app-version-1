460dc83d0db42930bf8726c54b2a0ab54257eca5
Nathan Rossow <nathanrossow@gmail.com>
2024-11-29 03:19:30 -0600

    Create cfpb-efficiency.py

---
+++ cfpb-efficiency.py
@@ -0,0 +1,31 @@
+# The Consumer Financial Protection Bureau (CFPB) employs around 1,500 people. This number can fluctuate slightly due to hiring, retirements, and other factors.
+
+----
+
+# salaries
+# The median salary for an employee at the Consumer Financial Protection Bureau (CFPB) is approximately $107,271 per year. This figure can vary based on factors such as job role, experience, and location.
+
+----
+
+# Breakdown of postions @ the CFPB: 
+
+# The average salary for employees at the Consumer Financial Protection Bureau (CFPB) varies depending on the position and level of experience. Here are some general figures:
+
+# Overall Average Salary: Approximately $95,000 per year.
+
+# Financial Analyst: Around $91,514 per year.
+
+# Attorney, Government: Around $133,068 per year.
+
+# Bank Examiner: Around $105,147 per year.
+
+# Business Operations Specialist: Around $70,494 per year.
+
+# Compliance Analyst: Around $57,658 per year.
+
+# Data Analyst: Around $71,569 per year.
+
+----
+
+# per year spend on CPB Salary
+#  $160,906,500.00
