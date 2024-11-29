010786cadfa34b05cb8d8fc7835334db86095709
Nathan Rossow <nathanrossow@gmail.com>
2024-11-29 03:38:02 -0600

    Create health-care-efficiency.py

---
+++ health-care-efficiency.py
@@ -0,0 +1,2 @@
+# health care 
+# https://www.msn.com/en-us/health/other/trump-health-appointee-suggested-obese-people-pay-more-health-insurance/ar-AA1uVwi4?ocid=msedgntp&cvid=7910d94c741d4282a9c6a7ba5524094d&ei=54
