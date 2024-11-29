d90b11aa846d6f50410a91e3ada8636d63552024
Nathan Rossow <nathanrossow@gmail.com>
2024-11-29 15:04:01 -0600

    Create us-econmic-displacement.py

---
+++ us-econmic-displacement.py
@@ -0,0 +1,7 @@
+# how many employees were out of a job since 11,240 companies went out of business in 2024
+
+# To estimate the number of employees affected, we can use the average number of employees per company. Based on available data, the average small business has around 132 employees.
+
+# Using this average, we can estimate that approximately 1,485,680 employees (11,240 companies * 132 employees per company) were affected by these bankruptcies in 2024.
+
+# It's a significant number and highlights the impact of business failures on employment. Is there a specific industry or region you're particularly concerned about?
